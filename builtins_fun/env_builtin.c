25b9783a8bbf5d3f8e779c6601326793abcb8978
Abderrahmane Sebban <asebban@e1r10p14.1337.ma>
2025-05-21 17:13:22 +0100

    test

--- builtins_fun/env_builtin.c
+++ builtins_fun/env_builtin.c
@@ -6,7 +6,7 @@
 /*   By: asebban <asebban@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/21 12:14:24 by asebban           #+#    #+#             */
-/*   Updated: 2025/05/21 17:08:14 by asebban          ###   ########.fr       */
+/*   Updated: 2025/05/21 17:12:22 by asebban          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,10 @@ int	env(t_shell *shell, char **args)
 	t_environ_node	*current;
 
 	if (args[1])
-    {
-        write(2, "env : Too many arguments\n", 26);
-        return 1;
-    }
+	{
+		write(2, "env : Too many arguments\n", 26);
+		return (1);
+	}
 	current = shell->env->head;
 	while (current)
 	{
